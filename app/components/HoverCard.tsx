d5843c08de3ca81c9a89057c053664c8de176e78
Volodymyr Trunov <iamvladimirtrunov@gmail.com>
2024-07-09 22:11:43 -0600

    Adjust HoverCard params

--- app/components/HoverCard.tsx
+++ app/components/HoverCard.tsx
@@ -6,10 +6,10 @@ const HoverCard: React.FC<
 > = ({ children, className, label }) => {
   return (
     <div className={cn([className, "group relative"])}>
-      <div className="group-hover:-translate-y-4 translate-0 transition-all duration-500">
+      <div className="group-hover:-translate-y-1/3 translate-0 transition-all duration-500">
         {children}
       </div>
-      <div className="group-hover:opacity-100 opacity-0 absolute font-medium text-textGray top-[calc(50%+20px)] text-sm w-full text-center transition-all duration-800">
+      <div className="group-hover:opacity-100 opacity-0 absolute font-medium text-textGray top-[65%] text-sm w-full text-center transition-opacity duration-500">
         {label}
       </div>
     </div>
